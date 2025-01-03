70dcf0b4c5d7711bef2effdd1586b0f04041ca9f
akashreddy-ops <danapanaakashreddy999@gmail.com>
2025-01-03 15:05:30 +0000

    datatypes-summary.js

--- .devcontainer/01_basics.js/datatypes-summary.js
+++ .devcontainer/01_basics.js/datatypes-summary.js
@@ -1,33 +1,33 @@
-  // primitive
+//   // primitive
 
-//  7 types : String, Number, Boolean, Null, Undefined, Symbol, Bigint
+// //  7 types : String, Number, Boolean, Null, Undefined, Symbol, Bigint
 
-const score = 100
-const scoreValue = 100.3
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = symbol('123')
-const anotherId = symbol('123')
+// const id = symbol('123')
+// const anotherId = symbol('123')
 
-console.log(id === anotherId);
-
-
-const bigNumber = 23121421426142751455152416257615275n
+// console.log(id === anotherId);
 
 
+// const bigNumber = 23121421426142751455152416257615275n
 
-// Reference (Non primitive)
 
-// Array, Objects, Functions
 
-const heros = ["Ramcharan", "Alluarjun", "Prabhs"];
-let myObj =  {
-    name: "Akash",
-    age: 19,
-}
+// // Reference (Non primitive)
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+// // Array, Objects, Functions
+
+// const heros = ["Ramcharan", "Alluarjun", "Prabhs"];
+// let myObj =  {
+//     name: "Akash",
+//     age: 19,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
